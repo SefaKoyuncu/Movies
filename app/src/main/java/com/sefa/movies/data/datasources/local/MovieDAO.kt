044b989327bd4f42c3a444bd609451b24506f1b9
SefaKoyuncu <sefakoyuncu1@outlook.com>
2024-01-03 16:53:23 +0300

    database functions optimized on DAO.

--- app/src/main/java/com/sefa/movies/data/datasources/local/MovieDAO.kt
+++ app/src/main/java/com/sefa/movies/data/datasources/local/MovieDAO.kt
@@ -12,14 +12,9 @@ interface MovieDAO
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun upsertMovie(movie: Movie) //update and insert
 
-    @Query("SELECT COUNT(*) FROM movies WHERE id = :movieID")
-    suspend fun getMovieCount(movieID: Int): Int
-
-    @Query("SELECT EXISTS (SELECT 1 FROM movies WHERE id = :movieID)")
-    suspend fun isMovieExist(movieID: Int): Boolean {    // movieID varsa true yoksa false döncek
-        return getMovieCount(movieID) > 0
-    }
-
     @Query("DELETE FROM movies WHERE id = :movieID")
     suspend fun deleteMovie(movieID: Int)
+
+    @Query("SELECT EXISTS (SELECT 1 FROM movies WHERE id = :movieID) AS RESULT")
+    suspend fun isMovieExist(movieID: Int): Boolean
 }
